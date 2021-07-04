d4f7fdfecc58a8b11ed5946d6a797b1ca6e1f773
TheCloudkicker <ckcameron1@gmail.com>
2021-07-04 13:57:57 -0400

    Fixed nginx config

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the world of madness2
+          Welcome to the world of madness22
         </p>
         <a
           className="App-link"
