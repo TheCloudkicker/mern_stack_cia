9190ea2a50eb6eee2005de88d0101e9d4efd5a3a
TheCloudkicker <ckcameron1@gmail.com>
2021-07-05 14:25:46 -0400

    Added input Validator Tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { validateString } from './components/validateInputs'
+
+test('input validator works with spaces', () => {
+  const testString = 'the boston red sox'
+  return validateString(testString).then(data => {
+    expect(data.parsedArray).toEqual(['the', 'boston', 'red', 'sox'])
+  })
+})
+
+test('input validator works with commas', () => {
+  const testString = 'the,boston,red,sox'
+  return validateString(testString).then(data => {
+    expect(data.parsedArray).toEqual(['the', 'boston', 'red', 'sox'])
+  })
+})
+
+test('input validator works with double quotes', () => {
+  const testString = 'the boston "red sox"'
+  return validateString(testString).then(data => {
+    expect(data.parsedArray).toEqual(['the', 'boston', 'red sox'])
+  })
+})
+
+test('input validator errors out if odd number of double quotes', () => {
+  const testString = 'the boston "red sox'
+  return validateString(testString).then(data => {
+    expect(data.status).toEqual(false)
+  })
+})
